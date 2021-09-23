97d6d1ba5e19764ecece3b239a081b5f840cf43e
Vesper Williams <veswill3@gmail.com>
2021-09-23 11:38:52 -0600

    update key repeat

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -10,8 +10,9 @@
 # Disable press-and-hold for keys in favor of key repeat.
 defaults write -g ApplePressAndHoldEnabled -bool false
 
-# Set a really fast key repeat.
+# Set a blazingly fast keyboard repeat rate
 defaults write NSGlobalDomain KeyRepeat -int 1
+defaults write NSGlobalDomain InitialKeyRepeat -int 10
 
 # Disable the “Are you sure you want to open this application?” dialog
 defaults write com.apple.LaunchServices LSQuarantine -bool false
