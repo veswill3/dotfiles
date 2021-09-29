4b8257d617336142a8eb091f47516d0f4a0cabca
Vesper Williams <veswill3@gmail.com>
2021-09-29 15:37:33 -0600

    add direnv and pre-commit update python setup

--- python/config.zsh
+++ python/config.zsh
@@ -1,5 +1,9 @@
-# setup pyenv and pyenv-virtualenv'
+# Compiler Flags
+export LDFLAGS="-L/opt/homebrew/opt/node@14/lib -L/opt/homebrew/opt/libpq/lib -L/opt/homebrew/Cellar/openssl@1.1/1.1.1l/lib"
+export CPPFLAGS="-I/opt/homebrew/opt/node@14/include -I/opt/homebrew/opt/libpq/include -I/opt/homebrew/Cellar/openssl@1.1/1.1.1/include"
+# export PKG_CONFIG_PATH="/opt/homebrew/opt/libpq/lib/pkgconfig"
 
+# setup pyenv and pyenv-virtualenv'
 eval "$(pyenv init --path)" # TODO: this should move to .zprofile and not .zshrc... but how?
 eval "$(pyenv init -)"
 eval "$(pyenv virtualenv-init -)"
