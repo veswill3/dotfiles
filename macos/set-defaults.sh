6a7ae748f8fd8e832e2aef4e5f6f1b5a92406e0e
Vesper Williams <veswill3@gmail.com>
2021-11-23 10:15:26 -0700

    slow down key repeat a bit

--- macos/set-defaults.sh
+++ macos/set-defaults.sh
@@ -11,8 +11,8 @@
 defaults write -g ApplePressAndHoldEnabled -bool false
 
 # Set a blazingly fast keyboard repeat rate
-defaults write NSGlobalDomain KeyRepeat -int 1
-defaults write NSGlobalDomain InitialKeyRepeat -int 10
+defaults write NSGlobalDomain KeyRepeat -int 2
+defaults write NSGlobalDomain InitialKeyRepeat -int 15
 
 # Disable the “Are you sure you want to open this application?” dialog
 defaults write com.apple.LaunchServices LSQuarantine -bool false
