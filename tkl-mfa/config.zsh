45c6f9fefc07f7dc11c1fbd5e1cdf9b56cf1571c
Vesper Williams <veswill3@gmail.com>
2022-06-20 09:51:53 -0600

    upgrade tkl-mfa

--- tkl-mfa/config.zsh
+++ tkl-mfa/config.zsh
@@ -1,6 +1,7 @@
 export AWS_MFA_1PASSWORD_ITEM=Amazon
 source "$HOME/.opsignin"
 opsignin() {
-  op signin tackle >! "$HOME/.opsignin"
+  op signin -f >! "$HOME/.opsignin"
   source "$HOME/.opsignin"
+  tkl-mfa
 }
