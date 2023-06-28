451bf71cb2674c5ffd1db73984f4f3e209b7e0f3
Vesper Williams <veswill3@gmail.com>
2023-06-28 15:58:22 -0600

    [python] append to CPPFLAGS

--- python/config.zsh
+++ python/config.zsh
@@ -1,6 +1,6 @@
 # Compiler Flags
 export LDFLAGS="-L/opt/homebrew/opt/node@14/lib -L/opt/homebrew/opt/libpq/lib -L/opt/homebrew/Cellar/openssl@1.1/1.1.1l/lib"
-export CPPFLAGS="-I/opt/homebrew/opt/node@14/include -I/opt/homebrew/opt/libpq/include -I/opt/homebrew/Cellar/openssl@1.1/1.1.1/include"
+export CPPFLAGS="-I/opt/homebrew/opt/node@14/include -I/opt/homebrew/opt/libpq/include -I/opt/homebrew/Cellar/openssl@1.1/1.1.1/include $CPPFLAGS"
 # export PKG_CONFIG_PATH="/opt/homebrew/opt/libpq/lib/pkgconfig"
 
 # setup pyenv and pyenv-virtualenv'
