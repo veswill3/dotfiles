df56bca799d2a397c60fefbe6884f7d027a3ef2b
Vesper Williams <veswill3@gmail.com>
2025-06-03 23:35:14 -0600

    just some cleanup before leaving tackle lots of old and maybe unused stuff

--- python/config.zsh
+++
@@ -1,9 +0,0 @@
-# Compiler Flags
-export LDFLAGS="-L/opt/homebrew/opt/node@14/lib -L/opt/homebrew/opt/libpq/lib -L/opt/homebrew/Cellar/openssl@1.1/1.1.1l/lib"
-export CPPFLAGS="-I/opt/homebrew/opt/node@14/include -I/opt/homebrew/opt/libpq/include -I/opt/homebrew/Cellar/openssl@1.1/1.1.1/include $CPPFLAGS"
-# export PKG_CONFIG_PATH="/opt/homebrew/opt/libpq/lib/pkgconfig"
-
-# setup pyenv and pyenv-virtualenv'
-eval "$(pyenv init --path)" # TODO: this should move to .zprofile and not .zshrc... but how?
-eval "$(pyenv init -)"
-eval "$(pyenv virtualenv-init -)"
